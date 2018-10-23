465d16f919658cb7b45eadaa4ee9acd8fd7feac8
aliakbar09a <aliakbar09a@gmail.com>
2018-10-23 18:01:01 +0530

    brain added

--- brain.py
+++ brain.py
@@ -1,4 +1,12 @@
 import random
+import numpy as np
 
 class brain:
-    def __init__(self, )
+    self.weights = []
+    def __init__(self, layers):
+        self.layers = layers
+        for i in range(len(layers) - 1):
+            theta = np.random.rand(layers[i], layers[i+1])
+            self.weights.append(theta)
+
+b = brain([12, 32, 3])
